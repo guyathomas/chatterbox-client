69bc153425090745e7d7ce507a6389288900ba50
stolemyusername <guythomas721@gmail.com>
2016-09-26 15:29:29 -0700

    got messages to load on screen

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,20 @@
-// YOUR CODE HERE:
+$(document).ready(function() {
+  $.ajax({
+    type: 'GET',
+    url: 'https://api.parse.com/1/classes/messages',
+    success: function(messages) {
+      var array = messages.results;
+      for (var i = 0; i < array.length; i++) {
+        var html = htmlTemplate(array[i]);
+        $('#main').append(html);
+      }
+    }
+  });
+});
+
+var htmlTemplate = function (message) {
+  var msgPanel = $('<div class="msgPanel"></div>');
+  msgPanel.append('<p>' + message.username + '</p>');
+  msgPanel.append('<p>' + message.text + '</p>');
+  return msgPanel;
+};
