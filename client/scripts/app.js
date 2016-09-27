da0a4bbc13a5c8aed2e638575920935dd067af6d
stolemyusername <guythomas721@gmail.com>
2016-09-26 20:39:25 -0700

    Filter by room name

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,44 +2,102 @@
 /*=====================================
 ===============GET request=============
 ======================================*/
+var roomNames = [];
+var friends = [];
 $(document).ready(function() {
+
   $.ajax({
     type: 'GET',
     url: 'https://api.parse.com/1/classes/messages',
-    data: {"order":"-createdAt"},
+    data: {'order': '-createdAt'},
     success: function(messages) {
       var array = messages.results;
       for (var i = 0; i < array.length; i++) {
         var html = htmlTemplate(array[i]);
         $('.messageContainer').prepend(html);
       }
+
+      //Create the room dropdown menu
+      roomNames = getRoomNames(array);
+      $select = $('.dropdown');
+      for (room in roomNames) {
+        var option = $('<option value="' + roomNames[room] + '"' + '>' + roomNames[room] + '</option>');
+        $select.append(option);
+      }
     }
   });
+  
   $('#submitBtn').on('click', function(event) {
     event.preventDefault();
     var newMessage = $('#newMessage').val();
     postMessage(newMessage);
   });
+
+  $( '.dropdown' ).change(function(event) {
+    $.ajax({
+      type: 'GET',
+      url: 'https://api.parse.com/1/classes/messages',
+      data: {'order': '-createdAt'},
+      success: function(messages) {
+        var array = messages.results;
+        var room = $('.dropdown').val();
+        var roomArray = _.filter(array, function(item) { return item.roomname === room; });
+        for (var i = 0; i < roomArray.length; i++) {
+          var html = htmlTemplate(roomArray[i]);
+          $('.messageContainer').prepend(html);
+        }
+      }
+    });
+  });
+
+  $('.messageContainer').on('click', '.username',  function (event) {
+    var name = this.innerHTML;
+    if (friends.indexOf(name) === -1) {
+      friends.push(this.innerHTML);      
+    }
+
+  });
+
 });
+
+//Get all room names into an array
+var getRoomNames = function(array) {
+  var storage = ['All rooms'];
+  for (var i = 0; i < array.length; i++) {
+    var element = array[i].roomname;
+    if (storage.indexOf(element) === -1 && element) {
+      storage.push(element);
+    }
+  }
+  return storage;
+};
 
 // Template for sticking message on HTML  
 
-var htmlTemplate = function (message) {
+var htmlTemplate = function (message, friend) {
   var msgPanel = $('<div class="msgPanel"></div>');
-  msgPanel.append('<p>' + message.username + '</p>');
+  
+  //Add friend styling
+  if (friend) {
+    msgPanel.append('<p class="username friend">' + message.username + '</p>');    
+  } else {
+    msgPanel.append('<p class="username">' + message.username + '</p>');    
+  }
   msgPanel.append('<p>' + htmlEscape(message.text) + '</p>');
+  
   return msgPanel;
 };
 
 //Escaping the messages we recieve so we don't get trolled
 var htmlEscape = function (str) {
-  console.log("str: ", str);
+  if (!(str)) { return ''; }
+
   return str
-    .replace(/&/g, '&amp;')
-    .replace(/"/g, '&quot;')
-    .replace(/'/g, '&#39;')
-    .replace(/</g, '&lt;')
-    .replace(/>/g, '&gt;');
+  .replace(/&/g, '&amp;')
+  .replace(/"/g, '&quot;')
+  .replace(/'/g, '&#39;')
+  .replace(/</g, '&lt;')
+  .replace(/>/g, '&gt;');
 };
 
 
@@ -66,6 +124,14 @@ var postMessage = function(message) {
     }
   });
 };
+
+
+//Spam control
+//Escape pink man
+//Waiting image while refreshing
+//Add friends
+//See messages by friends
+// save friends so they are not forgotten on reload
 
 
 
